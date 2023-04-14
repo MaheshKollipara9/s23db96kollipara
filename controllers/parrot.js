99d8bfab66e3372f0bee90cbaf561cf46ed93418
Mahesh Kollipara <123134965+S558996@users.noreply.github.com>
2023-04-13 20:42:11 -0500

    ss8

--- controllers/parrot.js
+++ controllers/parrot.js
@@ -15,9 +15,25 @@ exports.parrot_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: parrot detail: ' + req.params.id);
 };
 // Handle parrot create on POST.
-exports.parrot_create_post = function(req, res) {
-res.send('NOT IMPLEMENTED: parrot create POST');
-};
+exports.parrot_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new parrot();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"parrot_type":"goat", "cost":12, "size":"large"}
+    document.parrot_color = req.body.parrot_color;
+    document.parrot_weight = req.body.parrot_weight;
+    document.parrot_cost = req.body.parrot_cost;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
 // Handle parrot delete form on DELETE.
 exports.parrot_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: parrot delete DELETE ' + req.params.id);
